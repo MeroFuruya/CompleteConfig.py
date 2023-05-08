073f791322b9dcff0311beebf806585f6e1be1e7
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2023-05-08 16:17:49 +0200

    rename variable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CompleteConfig import *
 
 # define your own config
-write_config = {
+config_dict = {
     "std_key": "std_value",
     StrKey("str_key"): "str_value",
     Key("comment_key", "comment_key_comment"): "comment_value",
@@ -43,20 +43,20 @@ write_config = {
 }
 # and store it to a file
 with open("test.conf", "w") as f:
-    dump(f, write_config)
+    dump(f, config_dict)
 
 
 
 # open an existing config
 with open("test.conf", "r") as f:
-    read_config = load(f)
+    config_dict = load(f)
 
-# and change something
-read_config["std_key"] = "new_std_value"
-read_config["dict_str_key"]["dict_std_key"] = "new_dict_std_value"
+# change something
+config_dict["std_key"] = "new_std_value"
+config_dict["dict_str_key"]["dict_std_key"] = "new_dict_std_value"
 
 # and save your changes to the file
 with open("test.conf", "w") as f:
-    dump(f, read_config)
+    dump(f, config_dict)
 
 # Path: main.py
